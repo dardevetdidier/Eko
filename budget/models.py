e6ec2f6209cee7704995bb49cab8200b376be2c8
Didier Dardevet <didier.dardevet@gmail.com>
2022-02-11 16:09:03 +0100

    add models

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Deposit(models.Model):
+    description = models.CharField(max_length=150)
+    amount = models.FloatField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Deposit: {self.amount} : {self.date_created}"
+
+
+
+class Withdraw(models.Model):
+    description = models.CharField(max_length=150)
+    amount = models.FloatField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Withdraw: {self.amount} : {self.date_created}"
+
+
+class Account(models.Model):
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    balance = models.FloatField()
+    deposit = models.ForeignKey(to=Deposit, on_delete=models.CASCADE)
+    withdraw = models.ForeignKey(to=Withdraw, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# TODO 1 - register mod
+
