8107b0d61efeee574c347bd001323daf6a62cf43
Didier Dardevet <didier.dardevet@gmail.com>
2022-02-12 00:37:35 +0100

    Complete login page

--- budget/views.py
+++ budget/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import LoginForm
 
@@ -10,13 +10,19 @@ def index(request):
     Display a login form and checks if user exists then log in.
     """
     login_form = LoginForm()
-    if request.method == 'Post':
+    if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
 
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
+            return redirect('budget')
         else:
-            messages.info(request, "Erreur dans le nom d'utilisateur ou le mot de passe")
+            messages.info(request, "Erreur dans le nom d'utilisateur et/ou le mot de passe")
+
     return render(request, 'budget/index.html', context={'form': login_form})
+
+
+def budget(request):
+    return render(request, 'budget/budget.html')
