2919fabb12e632a1cd68cdd18759e3ccde49d576
Didier Dardevet <didier.dardevet@gmail.com>
2022-02-12 10:02:56 +0100

    Register models / add readme file

---
+++ README.md
@@ -0,0 +1,6 @@
+# EKO - Budget management application
+___
+
+A personal Django projet.
+
+Work in progess...
--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Deposit, Withdraw, Account, Category
 
-# Register your models here.
+
+@admin.register(Deposit)
+class DepositAdmin(admin.ModelAdmin):
+    list_display = ['description', 'amount', 'date_created']
+
+
+@admin.register(Withdraw)
+class DepositAdmin(admin.ModelAdmin):
+    list_display = ['description', 'amount', 'category', 'date_created']
+
+
+@admin.register(Category)
+class DepositAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Account)
+class DepositAdmin(admin.ModelAdmin):
+    list_display = ['name', 'balance', 'owner']
